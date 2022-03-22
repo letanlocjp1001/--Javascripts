2eda10d6ee89acc0252ea1914f38b3f288993972
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-22 11:26:50 +0900

    update

--- 01/script.js
+++ 01/script.js
@@ -1,9 +1,71 @@
-let js = 'amazing'
-if (js === 'amazing') alert('JavaScript is FUN')
+// let js = 'amazing'
+// if (js === 'amazing') alert('JavaScript is FUN')
 
-console.log(40 + 8 + 23 - 10)
+// console.log(40 + 8 + 23 - 10)
 
-console.log('Kaneki')
+// console.log('Kaneki')
 
-let firstName = 'Kaneki'
-console.log(firstName)
+// let firstName = 'Kaneki'
+// console.log(firstName)
+
+// let javascriptInFun = true
+// console.log(typeof javascriptInFun)
+// console.log(typeof 23)
+// console.log(typeof 'Kaneki')
+
+// javascriptInFun = 'yes'
+// console.log(javascriptInFun)
+
+// let year
+// console.log(year)
+// console.log(typeof year)
+
+// year = 1994
+// console.log(typeof year)
+
+// console.log(typeof null)
+
+// const now = 2023
+
+// const ageKaneki = now - 1994
+// const ageSarah = now - 2018
+
+// console.log(ageKaneki, ageSarah)
+
+// console.log(ageKaneki * 2, ageKaneki / 10, 2 ** 3)
+// //2**3 = 2*2*2
+
+// const firstName = 'Kaneki'
+// const lastName = 'Ken'
+
+// console.log(firstName + ' ' + lastName)
+
+// let x = 10 + 5
+// x += 10 //x= x + 10
+// x *= 4 //x= x * 4
+// x++ //x = x+1
+// x-- //x = x-1
+// x-- //x = x-1
+// console.log(x)
+
+const firstName = 'Kaneki'
+const job = 'Teacher'
+const birthYear = 1994
+const year = 2037
+
+const kaneki =
+  "I'm " + firstName + ', a ' + (year - birthYear) + 'year old ' + job + '!'
+console.log(kaneki)
+
+const kanekiNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`
+console.log(kanekiNew)
+
+console.log(`Just a regular string...`)
+
+console.log('String with \n\
+multiple \n\
+line ')
+
+console.log(`String
+multiple
+lines`)
