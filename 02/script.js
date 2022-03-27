29a15b3ffe60e769040a2977b2d09673861c484a
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-28 08:13:51 +0900

    function

--- 02/script.js
+++ 02/script.js
@@ -1,7 +1,73 @@
 'use strict'
-
+/*
 let hasDriversLicense = false
 const passTest = true
 
 if (passTest) hasDriversLicense = true
 if (hasDriversLicense) console.log('I can drive ')
+*/ /////////////
+
+// function logger() {
+//   console.log('My Name Kaneki')
+// }
+
+// // calling / running / invoking function
+// logger()
+// logger()
+// logger()
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`
+//   return juice
+// }
+
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
+
+// const appleOrangeJuice = fruitProcessor(2, 4)
+// console.log(appleOrangeJuice)
+
+// const num = Number('23')
+
+// function declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear
+// }
+// const age1 = calcAge1(1991)
+
+// // function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear
+// }
+// const age2 = calcAge2(1995)
+// console.log(age1, age2)
+
+//Arrow function
+
+// const calcAge3 = (birthYear) => 2037 - birthYear
+// const age3 = calcAge3(1999)
+// console.log(age3)
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear
+//   const retirement = 65 - age
+//   // return retirement
+//   return `${firstName} retires in ${retirement} years`
+// }
+
+// console.log(yearsUntilRetirement(1993, 'Kaneki'))
+// console.log(yearsUntilRetirement(1988, 'Sarad'))
+
+// function cutFuitPieces(fruit) {
+//   return fruit * 4
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFuitPieces(apples)
+//   const orangesPieces = cutFuitPieces(oranges)
+
+//   const juice = `Juice with ${applePieces} piece and ${orangesPieces} piece`
+//   return juice
+// }
+
+// console.log(fruitProcessor(2, 3))
