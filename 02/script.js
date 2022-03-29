991bb159829782bbfd8a0b2f1586611befe1d8cf
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-29 12:28:16 +0900

    update

--- 02/script.js
+++ 02/script.js
@@ -71,3 +71,66 @@ if (hasDriversLicense) console.log('I can drive ')
 // }
 
 // console.log(fruitProcessor(2, 3))
+// const calcAge = (birthYear) => {
+//   return 2037 - birthYear
+// }
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = calcAge(birthYear)
+//   const retirement = 65 - age
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`)
+//     return retirement
+//   } else {
+//     console.log(`${firstName} has already retired`)
+//     return -1
+//   }
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Kaneki'))
+// console.log(yearsUntilRetirement(1950, 'Sahra'))
+
+// Arrays
+
+// const friend1 = 'Michael'
+// const friend2 = 'Steven'
+// const friend3 = 'Peter'
+
+// // method 1
+// const friends = ['Michael', 'Steven', 'Peter']
+// console.log(friends)
+// //method 2
+// const years = new Array(1991, 1994, 1990, 2000, 'kaneki')
+// // console.log(years)
+
+// console.log(friends[0])
+// console.log(friends[2])
+// console.log(friends.length)
+
+// console.log(friends[friends.length - 1])
+
+// friends[2] = 'Jay'
+// console.log(friends)
+// console.log(friends[friends.length - 1])
+
+// const firstName = 'Jonas'
+// const jonas = [firstName, 'Kaneki', 2037 - 1991, 'teacher', friends]
+
+// console.log(jonas)
+// console.log(jonas.length)
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear
+// }
+// const years1 = [1990, 1967, 2002, 2010, 2018]
+
+// const age = calcAge(years1[0])
+// const age1 = calcAge(years1[1])
+// const age2 = calcAge(years1[years1.length - 1])
+// console.log(age, age1, age2)
+
+const friends = ['Michael', 'Steven', 'Peter']
+const newLength = friends.push('Jay')
+console.log(friends)
+console.log(newLength)
