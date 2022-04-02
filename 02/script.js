a71cf93d1d83e2d3eb642391f5aa54d639de7669
LE TAN LOC <letanlocjp1001@gmail.com>
2022-04-02 23:59:20 +0900

    update

--- 02/script.js
+++ 02/script.js
@@ -269,20 +269,57 @@ if (hasDriversLicense) console.log('I can drive ')
 //   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
 // }
 
+// const jonasArray = [
+//   'Jonas',
+//   'Schmedtmann',
+//   2037 - 1991,
+//   'teacher',
+//   ['Michael', 'Peter', 'Steven'],
+//   true,
+// ]
+
+// const types = []
+// // console.log(jonasArray[0])
+// // console.log(jonasArray[1])
+// // // ...
+// // console.log(jonasArray[jonasArray.length - 1])
+
+// for (let i = 0; i < jonasArray.length; i++) {
+//   console.log(jonasArray[i], typeof jonasArray[i])
+
+//   //Filling types array
+//   // types[i] = typeof jonasArray[i]
+//   types.push(typeof jonasArray[i])
+// }
+
+// console.log(types)
+
+// const years = [1991, 2007, 1969, 2020]
+// const ages = []
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i])
+// }
+// console.log(ages)
+
+// // continue and break
+
+// console.log('----ONLY STRINGS====')
+// for (let i = 0; i < jonasArray.length; i++) {
+//   if (typeof jonasArray[i] !== 'string') continue
+//   console.log(jonasArray[i], typeof jonasArray[i])
+// }
+
+// console.log('----BREAK WITH NUMBER====')
+// for (let i = 0; i < jonasArray.length; i++) {
+//   if (typeof jonasArray[i] === 'number') break
+//   console.log(jonasArray[i], typeof jonasArray[i])
+// }
+
 const jonasArray = [
   'Jonas',
   'Schmedtmann',
   2037 - 1991,
   'teacher',
   ['Michael', 'Peter', 'Steven'],
-  true,
 ]
-
-// console.log(jonasArray[0])
-// console.log(jonasArray[1])
-// // ...
-// console.log(jonasArray[jonasArray.length - 1])
-
-for (let i = 0; i < jonasArray.length; i++) {
-  console.log(jonasArray[i], typeof jonasArray[i])
-}
